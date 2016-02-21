62a89d5597cd3068c6b2af316fb05901336719de
Maxcoh <blb98@drexel.edu>
2016-02-21 03:20:04 -0500

    info button

--- CleanWaterPhilly/app/src/main/java/com/cleanwater/axp/cleanwaterphilly/MapsActivity.java
+++ CleanWaterPhilly/app/src/main/java/com/cleanwater/axp/cleanwaterphilly/MapsActivity.java
@@ -3,11 +3,15 @@ package com.cleanwater.axp.cleanwaterphilly;
  * Created by Brendan Barnes on 2/20/16.
  */
 
+import android.content.Intent;
 import android.content.res.AssetManager;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -22,6 +26,7 @@ import java.io.IOException;
 public class MapsActivity extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private Button info;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +62,13 @@ public class MapsActivity extends FragmentActivity {
             // Try to obtain the map from the SupportMapFragment.
             mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
                     .getMap();
+            info = ((Button) findViewById(R.id.info));
+            info.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("http://www.phillywatersheds.org/residents"));
+                    startActivity(browserIntent);
+                }});
             // Check if we were successful in obtaining the map.
             if (mMap != null) {
                 setUpMap();
